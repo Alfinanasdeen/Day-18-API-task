c9c6f2793c559bb221aee366b5183e6152b548be
Alfina.M <145192885+Alfinanasdeen@users.noreply.github.com>
2023-12-21 21:12:35 +0530

    Update Dog-Facts.js

--- DogFacts-API/Dog-Facts.js
+++ DogFacts-API/Dog-Facts.js
@@ -1,14 +1,11 @@
 function getDogFact() {
-  // Make a request to the Dog API
+ 
   fetch("https://dogapi.dog/api/facts")
     .then((response) => response.json())
     .then((data) => {
-      // Update the DOM with the retrieved fact
       const factElement = document.getElementById("fact");
-
-      // Check if the data structure matches the expected format
       if (data && data.facts && data.facts.length > 0) {
-        factElement.textContent = data.facts[0]; // Display the first fact
+        factElement.textContent = data.facts[0];
       } else {
         factElement.textContent = "Failed to retrieve dog facts.";
       }
