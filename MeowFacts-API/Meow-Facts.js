ab33f529383ff78628f5888cfef307eeab97d2d5
Alfina.M <145192885+Alfinanasdeen@users.noreply.github.com>
2023-12-21 21:13:32 +0530

    Update Meow-Facts.js

--- MeowFacts-API/Meow-Facts.js
+++ MeowFacts-API/Meow-Facts.js
@@ -1,9 +1,8 @@
 function getMeowFact() {
-  // Make a request to the Meow Facts API
+ 
   fetch("https://meowfacts.herokuapp.com/")
     .then((response) => response.json())
     .then((data) => {
-      // Update the DOM with the retrieved fact
       const factElement = document.getElementById("fact");
       factElement.textContent = data.data;
     })
